8e24eedbad1f262f0833378615ab5698cdbaa38f
Carlos Soto Pérez <carlos348@outlook.com>
2019-11-18 12:17:53 -0600

    new fields in gifts

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ function renderNavbar(){
                 loggedInNavbar.hidden = false;
                 loggedInNavbar.querySelectorAll('img').forEach(element => {
                     element.src = user.imagen;
-                })
+                }); 
                 return;
             }
         }
@@ -36,6 +36,52 @@ function loadGift(callback){
     xhr.open('GET', `${BASE_URL}/gifts/${giftId}`);
     xhr.onload = callback(xhr)
     xhr.send();
+}
+
+function renderHorizontalUserCard(user){
+    let html = `
+        <div class="card mb-3 shadow-sm col-md-12">
+            <div class="row no-gutters">
+                <div class="col-md-3">
+                    <a href="${user.redirectURL}">
+                        <img src=".${user.imagen}" class="card-img" style alt="${user.username}">
+                    </a>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                            <h5 class="card-title">${user.nombre}</h5>
+                            <p class="card-text">${user.usernae}</p>
+                            <small>Puntaje <span>${user.puntaje}</span></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    return html;
+}
+
+function renderHorizontalGiftCard(gift){
+    let html = `
+        <div class="card mb-3 shadow-sm col-md-12">
+            <div class="row no-gutters">
+                <div class="col-md-2">
+                    <a href="${gift.redirectURL}">
+                        <img src="${gift.imagen[0]}" class="card-img" alt="${gift.imagen[0]}">
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">${gift.nombre}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><a href="">${gift.publicadoPor}</a></h6>
+                        <p class="card-text">${gift.descripcion}</p>
+                        <small>Publicado hace <span>${gift.estado} días</span></small>
+                        <small>Publicado hace <span>${gift.creacion} días</span></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    return html;
 }
 
 renderNavbar();
